0ec2a7704cca7a6355c641d316d81dcad0f6a26c
Kaleb Davis <kalebdavisgithub@gmail.com>
2015-09-08 21:13:48 -0400

    working pomodoro with countdown

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# pomodoro_timer
+# Pomodoro Timer
+
+My take on a pomodoro style timer, with 25 minutes of work and 5 minutes of rest.
+
+Just run ```ruby pomodoro.rb``` and answer the prompts, and boom, you're in a pomodoro.
---
+++ pomodoro.rb
@@ -0,0 +1,30 @@
+puts 'Is this a work (w) or a rest (r) block?'
+b = gets.chomp
+limit = 5
+label = ''
+if b == 'w'
+	limit = 25
+	puts 'What are you working on?'
+	label = gets.chomp
+	puts 'Work hard!'
+end
+# Timing using countdown
+clock = Time.at(limit * 60)
+while clock != Time.at(0)
+	clock -= 1
+	sleep(1)
+	puts clock.strftime "%M:%S"
+end
+
+# Timing using actual time
+# start = Time.now
+# total_time = Time.now - start
+# while(total_time < (limit * 60))
+# 	sleep(1)
+# 	if total_time.to_i % 60 == 0
+# 		puts "#{total_time.to_i / 60} minutes completed"
+# 	end
+# end
+system("aplay /usr/share/sounds/alsa/beep.wav")
+puts 'You finished your pomodoro!'
+puts "You worked on: #{label}" unless label.empty?
