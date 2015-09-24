cd6a10cf4ee637a865293f70da46a6d64f96d382
paradime <brw4824@rit.edu>
2015-09-24 13:11:01 -0400

    Added timestamp

--- pomodoro_logger.rb
+++ pomodoro_logger.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class PomodoroLogger
   attr_reader :log_file
   FILE_NAME = 'pom_log.txt'
@@ -7,13 +8,13 @@ class PomodoroLogger
 
   def write_task(task)
     @log_file = File.new(FILE_NAME, 'a+')
-    @log_file.write("#{task}\n")
+    @log_file.write("You completed: |#{task}| on #{Time.now.strftime('%m/%d/%y at %l:%M')}\n")
     @log_file.close
   end
 
   def get_last_item
     @log_file = File.new(FILE_NAME, 'a+')
-    last_line = @log_file.readlines[-1]
+    last_line = @log_file.readlines[-1].match(/\|(.*)\|/)[1]
     @log_file.close
     last_line
   end
